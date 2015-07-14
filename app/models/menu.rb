13275401a1ceaa3cabb8d07bff55d2062f3335a2
John Bullock <bullock.l.john@gmail.com>
2015-07-14 09:58:01 -0400

    Navbar that works.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -10,6 +10,12 @@ class Menu < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with MyValidator
   def children
-    Menu.where(menu_id: id)
+    Menu.where(menu_id: self.id)
+  end
+  def parent_id
+    self.menu_id
+  end
+  def parent
+    Menu.find self.menu_id
   end
 end

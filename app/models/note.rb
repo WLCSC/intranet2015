edcef5d09f93c9ff28e7f6ebe91725dd9eff07ab
John Bullock <bullock.l.john@gmail.com>
2015-07-26 14:37:11 -0400

    Changed the color, made it so you can't set the expiration date to today on a note, and made the notes header appear on the welcome view only if there are actually notes.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ActiveRecord::Base
   validates :title, :content, presence: true
   validate :expiration_date_cannot_be_in_the_past
   def expiration_date_cannot_be_in_the_past
-    errors.add(:expires, "can't be in the past") if
-    !expires.blank? and expires < Date.today
+    errors.add(:expires, "can't be today or in the past") if
+    !expires.blank? and expires <= Date.today
   end
 end
